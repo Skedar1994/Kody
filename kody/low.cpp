f764fb5dd96179365fd017c9d2ab0882fdb80ebc
rafiki53 <jacek.moszkowski@gmail.com>
2016-10-06 11:17:34 +0200

    Add files via upload

--- kody/low.cpp
+++ kody/low.cpp
@@ -1,70 +1,61 @@
 #include <bits/stdc++.h>
-#define MAXN 1000005
+#define rep(i, j, k) for(int i = (int) j; i < (int) k; i++)
+#define pb push_back
+#define all(v) v.begin(), v.end()
 typedef long long ll;
 typedef long double ld;
 using namespace std;
 const ll MOD = 1000000007;
 
 
-vector < vector < int > > V;
-vector < int > PA;//punkty artykulacji
-vector < pair < int, int > > M;//mosty
-int low[MAXN], d[MAXN], nr = 1;
+struct AB {
+    vector<vector<int> >nei;
+    vector<int> AP;
+    vector<pair<int, int> > B;
+    vector<int> low, d;
+    int n, nr;
 
-void DFS(int v,	int ojciec)//przy pierwszym odpaleniu ojciec = 0!
-{
-	int ile=0,pkt_art=false;
-	low[v]=d[v]=nr++;
-	for(int i=0; i<(int)V[v].size(); i++)
-	{
-		int sas = V[v][i];
-		if (sas != ojciec)
-		{
-			if (!d[sas])
-			{
-				DFS(sas,v);
-				ile++;
-				low[v]=min(low[v],low[sas]);
-				if (ojciec && low[sas]>=d[v]) 
-					pkt_art=true;
-				if (low[sas]>d[v]) 
-					M.push_back({v, sas}); // most 
-			}
-			else 
-				low[v]=min(low[v],d[sas]);
-		}
-	}
-	if (!ojciec && ile>1) 
-		pkt_art=true;
-	if (pkt_art) 
-		PA.push_back(v);
-}
-int main()
-{
-	ios_base::sync_with_stdio(0);
-	int n, m, a, b;
-	cin>>n>>m;
-	V.resize(n+1);
-	while(m--)
-	{
-		cin>>a>>b;
-		V[a].push_back(b);
-		V[b].push_back(a);
-	}
-	DFS(1, 0);
-	for(int i=1; i<=n; i++)
-		if (!d[i])
-		{
-			nr = 1;
-			DFS(i, 0);
-		}
-	sort(PA.begin(), PA.end());
-	cout << PA.size()<<"\n";
-	for(int i=0; i<(int)PA.size(); i++)
-		cout<<PA[i]<<" ";
-	
+    void DFS(int v, int parent) {
+        int cnt = 0;
+        low[v] = d[v] = nr++;
+        bool art_pt = false, seen_parent = false;
+        for(auto w : nei[v]) {
+            if(w != parent or seen_parent) {
+                if(!d[w]) {
+                    DFS(w, v);
+                    cnt++;
+                    low[v] = min(low[v], low[w]);
+                    if(parent and low[w] >= d[v]) {
+                        art_pt = true;
+                    }
+                    if(low[w] > d[v]) {
+                        B.pb({v, w});
+                        B.pb({w, v});
+                    }
+                } else {
+                    low[v] = min(low[v], d[w]);
+                }
+            } else {
+                seen_parent = true;
+            }
+        }
+        if(!parent and cnt > 1) {
+            art_pt = true;
+        }
+        if(art_pt) {
+            AP.pb(v);
+        }
+    }
 
-	return 0;
-}
-
-
+    AB(vector<vector<int> > ini_nei, int ini_n)
+        :nei{ini_nei}, n(ini_n) {
+            low.resize(n + 1); d.resize(n + 1, 0);
+            rep(i, 1, n + 1) {
+                if(!d[i]) {
+                    nr = 1;
+                    DFS(i, 0);
+                }
+            }
+            sort(all(AP));
+        }
+};
