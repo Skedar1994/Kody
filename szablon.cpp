6246ac7ae0187e00653c6198f179dc9fadb46820
skedar1994 <skedar1@o2.pl>
2016-10-06 11:10:37 +0200

    szablon

---
+++ szablon.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+#define rep(i,k,n) for(ll i= (ll) k;i< (ll) n;i++)
+#define all(v) (v).begin(), (v).end()
+#define SZ(v) (int)((v).size())
+#define pb push_back
+#define ft first
+#define sd second
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+const long long INF = 4e18L + 1;
+const int IINF = 2e9 + 1;
+const int limit = 1048576;
+
+using namespace std;
+
+#define LOCAL
+#ifndef LOCAL
+#define cerr if(0)cout
+#define endl "\n"
+#endif
+
+
+int main()
+{
+   // ios_base::sync_with_stdio(0);
+   // cin.tie(0);
+    return 0;
+}
