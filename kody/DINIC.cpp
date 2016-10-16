bcee29d5577e6cda36fcdb007f947b13058fccee
skedar1994 <skedar1@o2.pl>
2016-10-16 16:08:09 +0200

    flow fix

--- kody/DINIC.cpp
+++ kody/DINIC.cpp
@@ -1,67 +1,85 @@
 #include<bits/stdc++.h>
-#define rep(i,k,n) for(int i= (int) k;i< (int) n;i++)
+#define rep(i,k,n) for(ll i= (ll) k;i< (ll) n;i++)
+#define all(v) (v).begin(), (v).end()
+#define SZ(v) (int)((v).size())
 #define pb push_back
+#define ft first
+#define sd second
 typedef long long ll;
-const long long inf = 8223372036854775807ll;
+typedef unsigned long long ull;
+typedef long double ld;
+const long long INF = 4e18L + 1;
+const int IINF = 2e9 + 1;
+const int limit = 1048576;
+
 using namespace std;
 
-int source, sink, n;
-vector<int> starte, d;
-vector<vector<ll> > cap;
-vector<vector<int> > nei;
-
-inline bool bfs()
+struct Dinic
 {
-    d = vector<int>(n + 1, -1);
-    d[source] = 0;
-    vector<int> q = {source};
-    rep(i, 0, q.size())
-    {
-        int v = q[i];
-        for(auto w: nei[v])
-            if(d[w] == -1 && cap[v][w])
-            {
-                q.pb(w);
-                d[w] = d[v] + 1;
-            }
-    }
-    return d[sink] != -1;
-}
+	int n, source, sink;
+	vector < int > starte, d;
+	vector < vector<ll> > cap;
+	vector < vector<int> > nei;
 
-ll dfs(int v, ll mincap)
-{
-  if (!mincap || v == sink) return mincap;
-  ll fp=0;
-  rep(i, starte[v], nei[v].size())
-  {
-      if(!mincap)
-        break;
-      starte[v]++;
-      int w = nei[v][i];
-      if(d[w] == d[v] + 1)
-      {
-          ll f = dfs(w, min(mincap, cap[v][w]));
-          mincap -= f;
-          fp += f;
-          cap[v][w] -= f;
-          cap[w][v] += f;
-      }
-  }
-  return fp;
-}
-
-ll dinic()
-{
-    ll maxflow = 0;
-    while(bfs())
-    {
-        starte = vector<int>(n + 1, 0);
-        maxflow += dfs(source, inf);
-    }
-    return maxflow;
-}
-
+	inline bool bfs()
+	{
+		d = vector<int>(n + 1, -1);
+		d[source] = 0;
+		vector<int> q(1,source);
+		rep(i, 0, q.size())
+		{
+			int v = q[i];
+			for(auto w: nei[v])
+				if(d[w] == -1 && cap[v][w])
+				{
+					q.pb(w);
+					d[w] = d[v] + 1;
+				}
+		}
+		return d[sink] != -1;
+	}
+	ll dfs(int v, ll mincap)
+	{
+		if (!mincap || v == sink) return mincap;
+		ll fp=0;
+		rep(i, starte[v], nei[v].size())
+		{
+			if(!mincap)
+				break;
+			else
+				starte[v]=i;
+			int w = nei[v][i];
+			if(d[w] == d[v] + 1)
+			{
+				ll f = dfs(w, min(mincap, cap[v][w]));
+				mincap -= f;
+				fp += f;
+				cap[v][w] -= f;
+				cap[w][v] += f;
+			} 
+		}
+		return fp;
+	}
+	Dinic(int n, int source, int sink) : n(n), source(source), sink(sink), cap(n+1, vector < ll >(n+1, 0)), nei(n+1) 
+	{	
+	}
+	ll flow()
+	{
+		ll maxflow = 0;
+		while(bfs())
+		{
+			starte = vector<int>(n + 1, 0);
+			maxflow += dfs(source, INF);
+		}
+		return maxflow;
+	}
+	void add_edge(int from, int to, ll capacity)
+	{
+		nei[from].pb(to);
+		nei[to].pb(from);
+		cap[from][to] = capacity;
+	}
+};
 int main()
-{
-  return 0;
+{	
 }
