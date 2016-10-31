c5283cbc130cc9dba8642cb85d495b69ca0c47fc
rafiki53 <jacek.moszkowski@gmail.com>
2016-10-31 14:06:59 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,9 @@
+#Jeśli testowany program nazywa się "a", to generatorka musi nazywać się "gen_a", a brute "brute_a".
+#Na wejściu podajemy nazwę pliku i liczbę testów, które chcemy wykonać.
+#Program kończy działanie jeśli wykona wszystkie testy, bądź jeśli na pewnym teście program i brute dadzą inny wynik.
+#W tym drugim przypadku wypisze numer testu na konsolę, plik in będzie zawierał wejście na którym były różne wyniki, a pliki out wyjścia.
+#Nie wiem jak działa ten diff, pewnie trzeba uważać na białe znaki, ale to chyba nie jest wielki problem.
+
 #! /bin/bash
 PROG=$1
 BRUTE="brute_"$1
