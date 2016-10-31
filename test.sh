e56bb3ba458b03b2217ff5fc3f1e02a8a0f61beb
rafiki53 <jacek.moszkowski@gmail.com>
2016-10-31 15:47:44 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 #Jeśli testowany program nazywa się "a", to generatorka musi nazywać się "gen_a", a brute "brute_a".
 #Na wejściu podajemy nazwę pliku i liczbę testów, które chcemy wykonać.
+#Generatorka jest odpalana z parametrem równym liczbie testów, można go olać, można użyć.
 #Program kończy działanie jeśli wykona wszystkie testy, bądź jeśli na pewnym teście program i brute dadzą inny wynik.
 #W tym drugim przypadku wypisze numer testu na konsolę, plik in będzie zawierał wejście na którym były różne wyniki, a pliki out wyjścia.
 #Opcja -b w diff mówi, żeby ignorował liczbę powtórzeń białych znaków
