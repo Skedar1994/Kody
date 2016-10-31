d4c7ed709c41c60824434e5554b923f0459d4eb4
Skedar1994 <skedar1@o2.pl>
2016-10-31 13:27:16 +0100

    Update and rename Przydatne_funkcje to Przydatne_funkcje.cpp

--- Przydatne_funkcje.cpp
+++ Przydatne_funkcje.cpp
@@ -1,9 +1,12 @@
 
 //BITSET
-  bitset < 10 > B;
+	bitset < 10 > B;
 	B[3] = 1;
 	B[7] = 1;
-	
 	cout << B._Find_first(); //znajduje indeks pierwszego zapalonego bitu 
 	cout << B._Find_next(8); //znajduje indeks pierwszego zapalonego bitu wiekszy niz 8
-  //jesli nie znajdzie, to zwroci rozmiar bitsetu(10)
+  	//jesli nie znajdzie, to zwroci rozmiar bitsetu(10)
+//unique
+	vector < int > V = {1, 3, 2, 1};
+	sort(all(V));
+	V.resize( distance(V.begin(), unique(all(V))) );
