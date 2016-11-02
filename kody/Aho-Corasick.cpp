25c0c6f539792e664b7b39748e8f19b72de19fbe
Skedar1994 <skedar1@o2.pl>
2016-11-02 11:23:45 +0100

    Update Aho-Corasick.cpp

--- kody/Aho-Corasick.cpp
+++ kody/Aho-Corasick.cpp
@@ -1,18 +1,3 @@
-#include<bits/stdc++.h>
-#define rep(i,k,n) for(int i= (int) k;i< (int) n;i++)
-#define all(v) (v).begin(), (v).end()
-#define pb push_back
-#define ft first
-#define sd second
-typedef long long ll;
-typedef unsigned long long ull;
-typedef long double ld;
-typedef unsigned int uint;
-const long long INF = 4e18L + 1;
-const int IINF = 2e9 + 1;
-const int limit = 1048576;
-using namespace std;
-
 struct AhoCorasick{
     static const int CHARSET = 26;
     
@@ -122,10 +107,3 @@ struct dynamicAho{
         return res;
     }
 };
-
-int main()
-{
-    //     cin.tie(0);
-    ios_base::sync_with_stdio(0);
-    return 0;
-}
