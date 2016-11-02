671249fd8cc56ac195a63351064e1345cce1d12e
Skedar1994 <skedar1@o2.pl>
2016-11-02 11:31:34 +0100

    Update logartytm_dyskretny.cpp

--- kody/logartytm_dyskretny.cpp
+++ kody/logartytm_dyskretny.cpp
@@ -1,26 +1,3 @@
-#include<bits/stdc++.h>
-#define rep(i,k,n) for(ll i= (ll) k;i< (ll) n;i++)
-#define all(v) (v).begin(), (v).end()
-#define SZ(v) (int)((v).size())
-#define pb push_back
-#define ft first
-#define sd second
-typedef long long ll;
-typedef unsigned long long ull;
-typedef long long ld;
-const long long INF = 4e18L + 1;
-const int IINF = 2e9 + 1;
-const int limit = 1048576;
-
-using namespace std;
-
-
-#define LOCAL
-#ifndef LOCAL
-#define cerr if(0)cout
-#define endl "\n"
-#endif
-
 ll pot(ll a, ll b, ll mod)
 {
 	if (b == 0) return 1;
@@ -32,7 +9,7 @@ ll odwr(ll a, ll mod)
 	return pot(a, mod-2, mod);
 }
 
-ll log_dys(ll a, ll b, ll mod)
+ll log_dys(ll a, ll b, ll mod)//szukamy n, ze a^n = b mod mod. mod - pierwsze, 01 gdy nie ma
 {
 	a%=mod; b%=mod;
 	if (a == 0)
@@ -61,13 +38,4 @@ ll log_dys(ll a, ll b, ll mod)
 			return M[pom] + i * pier;
 	}
 	return -1;
-}
-
-
-int main()
-{
-	ll a, b, n;
-	cin>>a>>b>>n;
-	cout << log_dys(a, b, n);
-	return 0;
 }
