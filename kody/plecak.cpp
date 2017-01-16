9cb077dce31d260e7f5d8418d35ad33e6279493a
Skedar1994 <skedar1@o2.pl>
2017-01-16 11:42:13 +0100

    Create plecak.cpp

---
+++ kody/plecak.cpp
@@ -0,0 +1,34 @@
+/*
+dajemy przez referencje bitset w ktorym chcemy miec wynik i vector z rzeczami do zapakowania
+*/
+
+const int MAXN = 1000005;
+
+void pakuj(bitset < MAXN >& B, const vector < int >& V)
+{
+	B = 1;
+	map < int, int > M;
+	for(auto& el : V)
+		M[el]++;
+	for(auto& el : M)
+	{
+		if (el.sd < 200)
+		{
+			for(int i=0; i<el.sd; i++)
+				B |= (B<<el.ft);
+			continue;
+		}
+		vector < int > Ile(MAXN, IINF);
+		for(int i=0; i<MAXN-el.ft; i++)
+		{
+			if (B[i])
+				Ile[i] = 0;
+			Ile[i+el.ft] = min(Ile[i+el.ft], Ile[i] + 1);
+			if (Ile[i] <= el.sd) 
+				B[i] = true;
+		}
+		for(int i=MAXN-el.ft; i<MAXN; i++)
+			if (Ile[i] <= el.sd)
+				B[i] = true;
+	}
+}
