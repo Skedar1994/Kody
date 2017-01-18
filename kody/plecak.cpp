cb0e01dc279b2278a9d92623ed972a70326e01a5
rafiki53 <jacek.moszkowski@gmail.com>
2017-01-18 12:11:40 +0100

    Update plecak.cpp

--- kody/plecak.cpp
+++ kody/plecak.cpp
@@ -32,3 +32,18 @@ void pakuj(bitset < MAXN >& B, const vector < int >& V)
 				B[i] = true;
 	}
 }
+
+bitset<MAXN> pakuj(const vector < int >& V)
+{
+	bitset<MAXN> B = 1;
+	map < int, int > M;
+	for(auto& el : V)
+		M[el]++;
+    	for(auto& el : M){
+        	rep(_, 0, 2 - (el.sd & 1))
+            		B |= B << el.ft;
+        	if((el.sd >> 1) - 1 + (el.sd & 1))
+           		M[el.ft << 1] += (el.sd >> 1) - 1 + (el.sd & 1);
+   	}	
+    	return B;
+}
