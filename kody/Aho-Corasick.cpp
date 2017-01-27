3b83ee2c3203ae41f357a84426902448efc6a515
rafiki53 <jacek.moszkowski@gmail.com>
2017-01-27 10:50:14 +0100

    Update Aho-Corasick.cpp

--- kody/Aho-Corasick.cpp
+++ kody/Aho-Corasick.cpp
@@ -20,12 +20,11 @@ struct AhoCorasick{
     void add(const string& s, int val) {
         int v = 0;
         for(auto c : s) {
-            int& w = trie[v][c - 'a'];
-            if(w == -1) {
+            if(trie[v][c - 'a'] == -1) {
                 add_vertex();
-                w = size() - 1;
+                trie[v][c - 'a'] = size() - 1;
             }
-            v = w;
+            v = trie[v][c - 'a'];
         }
         tag[v]+=val;
     }
@@ -103,6 +102,64 @@ struct dynamicAho{
         int res = 0;
         for(auto& AC : onlineAho) {
             res += AC.count(s);
+        }
+        return res;
+    }
+};
+
+
+//pewnie szybsze ale gunwo troche
+namespace AhoCorasick{
+    const int MAX_SZ = 1e6, CHARSET = 26;
+    int trie[MAX_SZ + 10][CHARSET], tag[MAX_SZ + 10], fail[MAX_SZ + 10], bfs[MAX_SZ + 10];
+    int sz = 1;
+
+    void init(){ 
+        rep(i, 1, MAX_SZ + 10){
+            fail[i] = 1;
+        }
+    }
+
+    void add(const string& s, int val) {
+        int v = 1;
+        for(auto& c : s) {
+            if(trie[v][c - 'a'] == 0) {
+                trie[v][c - 'a'] = ++sz;
+            }
+            v = trie[v][c - 'a'];
+        }
+        tag[v] += val;
+    }
+
+    void build() {
+        int bfs_sz = 1;
+        for(auto& w : trie[1]) {
+            if(w != 0) {
+                bfs[bfs_sz++] = w;
+            } else {
+                w = 1;
+            }
+        }
+        
+        rep(i, 0, bfs_sz) {
+            int v = bfs[i];
+            tag[v] += tag[fail[v]];
+            rep(j, 0, CHARSET) {
+                if(trie[v][j] != 0) {
+                    fail[trie[v][j]] = trie[fail[v]][j];
+                    bfs[bfs_sz++] = trie[v][j];
+                } else {
+                    trie[v][j] = trie[fail[v]][j];
+                }
+            }
+        }
+    }
+
+    int count(const string& s){
+        int v = 0, res = 0;
+        for(auto& c : s){
+            v = trie[v][c - 'a'];
+            res += tag[v];
         }
         return res;
     }
