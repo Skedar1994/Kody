3e6f3406da8d72478535139b5e0b08a78cd733da
Skedar1994 <skedar1@o2.pl>
2017-01-31 17:38:26 +0300

    Update szablon.cpp

--- szablon.cpp
+++ szablon.cpp
@@ -13,12 +13,18 @@ const int IINF = 2e9 + 1;
 
 using namespace std;
 
-#define LOCAL
-#ifndef LOCAL
-#define cerr if(0)cout
-#define endl "\n"
-#endif
+template<class TH> void _dbg(const char *sdbg, TH h){ cerr<<sdbg<<'='<<h<<endl; }
+template<class TH, class... TA> void _dbg(const char *sdbg, TH h, TA... a) {
+  while(*sdbg!=',')cerr<<*sdbg++;cerr<<'='<<h<<','; _dbg(sdbg+1, a...);
+}
 
+#define LOCAL
+#ifdef LOCAL
+#define DBG(...) _dbg(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define DBG(...) (__VA_ARGS__)
+#define cerr if(0)cout
+#endif
 
 int main()
 {
