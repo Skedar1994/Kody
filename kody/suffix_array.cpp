8c88e8afd163479893fc841ba82bfcb3354f052a
Skedar1994 <skedar1@o2.pl>
2017-02-06 20:23:54 +0300

    Update suffix_array.cpp

--- kody/suffix_array.cpp
+++ kody/suffix_array.cpp
@@ -94,7 +94,26 @@ int input[N+5];
 int SA[N+5];
 string s;
 int n;
-  
+
+vector<int> lcp(const string& s, int* sa)
+{
+    int n=s.size(),k=0;
+    vector<int> lcp(n,0);
+    vector<int> rank(n,0);
+
+    for(int i=0; i<n; i++) rank[sa[i]]=i;
+
+    for(int i=0; i<n; i++, k?k--:0)
+    {
+        if(rank[i]==n-1) {k=0; continue;}
+        int j=sa[rank[i]+1];
+        while(i+k<n && j+k<n && s[i+k]==s[j+k]) k++;
+        lcp[rank[i]]=k;
+    }
+    return lcp;
+}
+
+
 int main(){
   cin>>n;
   cin>>s;
