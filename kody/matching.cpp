a585f562e36f25e8286ef9c16b2b1c41de5584ce
Skedar1994 <skedar1@o2.pl>
2017-03-12 17:22:47 +0100

    Update matching.cpp

--- kody/matching.cpp
+++ kody/matching.cpp
@@ -1,10 +1,16 @@
 //po usunieciu zmiennej zostalo jest zazwyczaj szybciej (moze nawet jakies 1.5 raza)
 //ale nie moge wtedy obiecac, ze na 100% bedzie zlozonosc O(msqrt(n))
 
+
 //1. W str[i] kazdy wierzcholek powinien miec nr 0 lub 1 -> w zaleznosci od tego, po ktorej stronie grafu dwudzielnego jest
 //2. Wierzcholki numerujemy od 1!
+//3. n na wejściu to liczba wierzchołków, nie trzeba (a jak szukamy maksymalnego
+//podzbioru niezaleznego to nie wolno) dawac n+1 na wejsciu
 
 //Matching m(..); m.matching() znajduje wynik 
+//mozna dodac! m.mpn() i to zwraca maksymalny podzbior niezalezny
+
+//mpn musze przetestowac dzis o 19.00 (12.03.17)
 
 struct Matching
 {
@@ -22,14 +28,14 @@ struct Matching
 		if (!str[v])
 		{
 			if (!para[v])
-				return para[v] = ojciec;
+				return (para[v] = ojciec);
 			if (DFS(para[v], v, zostalo-1))
-				return para[v] = ojciec;
+				return (para[v] = ojciec);
 			return false;
 		}
 		for(int i=0; i<SZ(V[v]); i++)
 			if (odl[ V[v][i] ] == odl[v] + 1 && !odw[ V[v][i] ] && DFS(V[v][i], v, zostalo-1))
-				return para[v] = V[v][i];
+				return (para[v] = V[v][i]);
 		return false;
 	}	
 	int sciezki() 
@@ -73,8 +79,43 @@ struct Matching
 	int matching()
 	{
 		int ile = 0, akt;
-		while(akt = sciezki())
+		while( (akt = sciezki()) )
 			ile+=akt;
 		return wynik = ile;
 	}
+	//to ponizej przepisujemy tylko gdy chcemy znalezc maksymalny podzbior niezalezny
+	//zwraca maksymalny podzbior niezalezny z wierzcholkow od 1 do n
+	vector < int > mpn()
+	{
+		vector < int > stan(n+1, 0);//0 - nieprzetworzony, 1 - wziety, -1 - nie moge wziac
+		queue < int > Q;
+		for(int i=1; i<=n; i++)
+		{
+			if(!para[i])
+			{
+				stan[i] = 1;
+				Q.push(i);
+			}
+		}
+		while(!Q.empty())
+		{
+			int x = Q.front();
+			Q.pop();
+			for(auto el : V[x])
+			{
+				if (stan[el]==0)
+				{
+					stan[el] = -1;
+					stan[ para[el] ] = 1;
+					Q.push(para[el]);
+				}
+			}
+		}
+		vector < int > odp;
+		for(int i=1; i<=n; i++)
+			if (stan[i] == 1 || (!stan[i] && str[i]))
+				odp.pb(i);
+		return odp;
+	}
+	
 };
