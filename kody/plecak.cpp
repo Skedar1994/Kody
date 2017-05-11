a74e28dba7e19513350fee0d915876bc3950433d
Skedar1994 <skedar1@o2.pl>
2017-05-11 21:50:55 +0300

    Update plecak.cpp

--- kody/plecak.cpp
+++ kody/plecak.cpp
@@ -1,14 +1,14 @@
-bitset<MAXN> pakuj(const vector < int >& V)
+bitset<MAXN> pakuj(const vector < pair <int, int> >& V)
 {
 	bitset<MAXN> B = 1;
 	map < int, int > M;
 	for(auto& el : V)
-		M[el]++;
-    	for(auto& el : M){
-        	rep(_, 0, 2 - (el.sd & 1))
-            		B |= B << el.ft;
-        	if((el.sd >> 1) - 1 + (el.sd & 1))
-           		M[el.ft << 1] += (el.sd >> 1) - 1 + (el.sd & 1);
-   	}	
-    	return B;
+		M[el.ft]+=el.sd;
+	for(auto& el : M){
+		rep(_, 0, 2 - (el.sd & 1))
+				B |= B << el.ft;
+		if((el.sd >> 1) - 1 + (el.sd & 1))
+			M[el.ft << 1] += (el.sd >> 1) - 1 + (el.sd & 1);
+	}	
+    return B;
 }
