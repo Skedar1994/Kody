364eb354a20a038fb1a110c3f16d38a2f0eba9e1
Skedar1994 <skedar1@o2.pl>
2017-06-07 23:15:44 +0200

    Update Bellman-Ford.cpp

--- kody/Bellman-Ford.cpp
+++ kody/Bellman-Ford.cpp
@@ -28,3 +28,30 @@ vector < ll > BF(int n, int s, vector < Trzy >& kr)
 		D.clear();
 	return D;
 }
+------------------------------------------------------------------------
+//zwraca pusty vector, gdy jest cykl ujemny
+//>INF/2 - nie ma drogi
+vector < ll > spfa(int n, int s, vector < vector < pair <int, int> > >& kr)
+{
+	vector < ll > D(n+1, INF);
+	vector < ll > Ile(n+1, 0);
+	D[s] = 0;
+	queue < int > Q;
+	Q.push(s);
+	while(!Q.empty())
+	{
+		int v = Q.front(); 
+		if (Ile[v]++ > n)
+			return {};
+		Q.pop();
+		for(auto& el : kr[v])
+		{
+			if (D[el.ft] > D[v] + el.sd)
+			{
+				Q.push(el.ft);
+				D[el.ft] = D[v] + el.sd;
+			}
+		}
+	}
+	return D;
+}
