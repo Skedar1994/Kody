1e97829576dadfd87f535202c224ba768cc32f8f
Skedar1994 <skedar1@o2.pl>
2017-06-30 02:24:48 +0300

    Create Bignum.cpp

---
+++ kody/Bignum.cpp
@@ -0,0 +1,110 @@
+//dl to logarytm 10 z baza
+struct Bignum
+{
+    ll baza = 1000000000000000LL, dl = 15;
+    vector < ll > V;
+    Bignum(ll x)
+    {
+        V = {x};
+        popraw();
+    }
+    Bignum()
+    {
+        V = {0};
+    }
+    Bignum(string s)
+    {
+        reverse(all(s));
+        V.resize(SZ(s)/dl+2);
+        vector < ll > Z = {1};
+        for(int i=1; i<=dl; i++)
+            Z.pb(Z.back() * 10);
+        //return;
+        for(int i=0; i<SZ(s); i++)
+        {
+            ll cyfra = s[i] - '0';
+            ll komorka = i/dl;
+            ll ile_zer = i%dl;
+            //cout << i << " " << komorka << endl;
+            V[komorka] += cyfra * Z[ile_zer];
+            //cout << i << endl;
+        }
+        //return;
+        popraw();
+    }
+    Bignum(string z, int a, int b)
+    {
+        string s;
+        for(int i=a; i<b; i++)
+            s += z[i];
+        (*this) = Bignum(s);
+    }
+
+    void popraw()
+    {
+        while(SZ(V) > 1 && V.back() == 0)
+            V.pop_back();
+        for(int i=0; i+1<SZ(V); i++)
+        {
+            if (V[i] >= baza)
+            {
+                V[i+1] += V[i]/baza;
+                V[i] %= baza;
+            }
+        }
+        while(V.back() >= baza)
+        {
+            int ile = V.back() / baza;
+            V.back() %= baza;
+            V.pb(ile);
+        }
+    }
+    bool operator<(const Bignum& b)
+    {
+        if (SZ(V) > SZ(b.V))
+            return false;
+        if (SZ(V) < SZ(b.V))
+            return true;
+
+        for(int i = SZ(V)-1; i>=0; i--)
+        {
+            if (V[i] > b.V[i])
+                return false;
+            if (V[i] < b.V[i])
+                return true;
+        }
+        return false;
+    }
+    Bignum& operator+=(const Bignum& b)
+    {
+        if (SZ(V) <= SZ(b.V))
+            V.resize(SZ(b.V));
+        for(int i=0; i<SZ(b.V); i++)
+            V[i] += b.V[i];
+        popraw();
+        return *this;
+    }
+    Bignum operator+(const Bignum& b)
+    {
+        Bignum res;
+        res = (*this);
+        res += b;
+        return res;
+    }
+    void wypisz()
+    {
+        cout << V.back();
+        //cout.width(dl);
+        //cout.fill('0');
+        for(int i = SZ(V)-2; i>=0; i--)
+        {
+            stringstream ss;
+            ss << V[i];
+            string s = ss.str();
+            while(SZ(s)< dl)
+                s = "0" + s;
+            cout << s;
+        }
+        //cout << endl;
+    }
+};
