d54eaabed58490b730e7bc23b44a3406e22df1ce
rafiki53 <jacek.moszkowski@gmail.com>
2017-09-27 12:45:15 +0200

    Update szablon.cpp

--- szablon.cpp
+++ szablon.cpp
@@ -18,6 +18,7 @@ template<class TH, class... TA> void _dbg(const char *sdbg, TH h, TA... a) {
   while(*sdbg!=',')cerr<<*sdbg++;cerr<<'='<<h<<','; _dbg(sdbg+1, a...);
 }
 
+//#undef LOCAL
 #ifdef LOCAL
 #define DBG(...) _dbg(#__VA_ARGS__, __VA_ARGS__)
 #else
