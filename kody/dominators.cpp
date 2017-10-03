9d3b334bb64845f4965d7e0be00d90e411e9de28
rafiki53 <jacek.moszkowski@gmail.com>
2017-10-03 12:00:32 +0200

    Update dominators.cpp

--- kody/dominators.cpp
+++ kody/dominators.cpp
@@ -1,16 +1,18 @@
+/*
+numeruje sie od zera, korzen to 0
+*/
 struct Dominators{
   int n_orig, n;
-  VI parent, semi, vertex, dom, ancestor, label;
-  vector<VI> succ, pred, bucket;
+  vector < int > parent, semi, vertex, dom, ancestor, label;
+  vector< vector < int > > succ, pred, bucket;
   
   Dominators(int _n):n_orig(_n), n(2 * (_n + 1)), parent(n), semi(n), vertex(n), dom(n), ancestor(n), label(n), succ(n), pred(n), bucket(n) {
     n = n_orig;
   }
   
   void add_edge(int a,int b){
-    debug(a,b);
     a++;b++;
-    succ[a].PB(b);
+    succ[a].pb(b);
   }
   
   void COMPRESS(int v) {
@@ -49,7 +51,7 @@ struct Dominators{
   }
 
   vector<int> doit() {
-    iota(ALL(label), 0);
+    iota(all(label), 0);
     DFS(1);
     for(int i=n; i>=2; --i) {
       int w = vertex[i];
@@ -79,7 +81,7 @@ struct Dominators{
     }
     dom[1]=0;
     vector<int> res(n_orig);
-    R(i,n_orig)res[i] = dom[i + 1] - 1;
+    rep(i,0,n_orig)res[i] = dom[i + 1] - 1;
     return res;
   }
 };
