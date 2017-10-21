4c606612078835833dd9d6f7146c2bd8a5b1abee
Skedar1994 <skedar1@o2.pl>
2017-10-21 18:01:14 +0200

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,22 @@
+int main(int argc, char* argv[])
+{
+	char* p=argv[1];
+	int test_num = stoi(argv[2]);
+	char b[1000];
+	
+	for(int i=1; i!=test_num; i++)
+	{
+		sprintf(b, "./gen_%s > %s.in", p, p);
+		system(b);
+		sprintf(b, "./%s < %s.in >%s.out", p, p, p);
+		system(b);
+		sprintf(b, "./brute_%s < %s.in > brute_%s.out", p, p, p);
+		system(b);
+		sprintf(b, "diff -b %s.out brute_%s.out", p, p);
+		if (system(b))
+		{
+			cout << i;
+			break;
+		}
+	}
+}
