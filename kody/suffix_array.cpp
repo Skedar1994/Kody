b235a533d98c491f792916c9b8a4d0319a70ec23
Skedar1994 <skedar1@o2.pl>
2017-11-03 07:33:28 +0100

    Update suffix_array.cpp

--- kody/suffix_array.cpp
+++ kody/suffix_array.cpp
@@ -128,8 +128,8 @@ struct SuffixArray {
   string s;
   vector<vector<int> > P;
   vector<pair<pair<int,int>,int> > M;
-
-  SuffixArray(const string &s) : L(s.length()), s(s), P(1, vector<int>(L, 0)), M(L) {
+  
+ SuffixArray(const string &_s) : L(_s.length()), s(_s), P(1, vector<int>(L, 0)), M(L) {
     for (int i = 0; i < L; i++) P[0][i] = int(s[i]);
     for (int skip = 1, level = 1; skip < L; skip *= 2, level++) {
       P.push_back(vector<int>(L, 0));
@@ -138,7 +138,8 @@ struct SuffixArray {
       sort(M.begin(), M.end());
       for (int i = 0; i < L; i++) 
 	P[level][M[i].second] = (i > 0 && M[i].first == M[i-1].first) ? P[level][M[i-1].second] : i;
-    }    
+    }
+    for(int i=0; i<L; i++) P.back()[i] = M[i].sd;    
   }
 
   vector<int> GetSuffixArray() { return P.back(); }
