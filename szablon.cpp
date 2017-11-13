df6874a74d75f6f65e7e173683632494ce106e8f
Skedar1994 <skedar1@o2.pl>
2017-11-13 17:09:40 +0100

    Update szablon.cpp

--- szablon.cpp
+++ szablon.cpp
@@ -8,20 +8,21 @@
 typedef long long ll;
 typedef unsigned long long ull;
 typedef long double ld;
-const long long INF = 1e18L + 1;
-const int IINF = 1e9 + 1;
+const long long INF = 4e18L + 1;
+const int IINF = 2e9 + 1;
 
 using namespace std;
 
 template<class TH> void _dbg(const char *sdbg, TH h){ cerr<<sdbg<<'='<<h<<endl; }
 template<class TH, class... TA> void _dbg(const char *sdbg, TH h, TA... a) {
-  while(*sdbg!=',')cerr<<*sdbg++;cerr<<'='<<h<<','; _dbg(sdbg+1, a...);
+  while(*sdbg!=',')cerr<<*sdbg++;
+  cerr<<'='<<h<<','; _dbg(sdbg+1, a...);
 }
 
 #ifdef LOCAL
 #define DBG(...) _dbg(#__VA_ARGS__, __VA_ARGS__)
 #else
-#define DBG(...) (__VA_ARGS__)
+#define DBG(...)
 #define cerr if(0)cout
 #endif
 
@@ -31,7 +32,6 @@ int main()
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 #endif
-  
-  
+
     return 0;
 }
