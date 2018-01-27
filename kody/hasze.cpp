d1dbc9ba2b0ccda61d898a398e6330b6ecb843e2
Skedar1994 <skedar1@o2.pl>
2018-01-27 14:31:52 +0100

    Update hasze.cpp

--- kody/hasze.cpp
+++ kody/hasze.cpp
@@ -41,3 +41,68 @@ struct Hasz
 	}
 };
 vector < ll > Hasz::P, Hasz::Q;
+
+
+//zmien litere, spytaj o hasz
+int VPOT[4] = {257, 293, 307, 313};
+ll pr = -1;
+
+struct HD
+{
+	const ll MOD = 1e9+7;
+	int pot = (1<<17);
+	vector < ll > tab;
+	vector < ll > P;
+	HD(int roz) : MOD(1e9+7)
+	{
+		pot = 1;
+		while(pot <= roz+2) pot *= 2;
+		tab.resize(2*pot);
+		P.resize(pot+1);
+		P[0] = 1;
+		srand((ll)(new int));
+		if (pr == -1) pr = VPOT[rand() % 4];
+		for(int i=1; i<SZ(P); i++) P[i] = P[i-1] * pr % MOD;
+	}
+	void ustaw(int gdzie, char co)
+	{
+		int aa = co - 'a'+1;
+		gdzie += pot;
+		tab[gdzie] = aa;
+		int wyk = 1;
+		
+		while(gdzie > 1)
+		{
+			gdzie/=2;
+			tab[gdzie] = (tab[2*gdzie]*P[wyk] + tab[2*gdzie+1]) % MOD;
+			wyk *= 2;
+		}
+	}
+
+	ll wez(int l, int p)
+	{
+		l += pot, p += pot;
+		if (l == p)
+			return tab[l];
+		
+		ll lh = tab[l]%MOD, ph = tab[p];
+		ll da = 1;//dl akt
+		ll dp = 1;//dl prawego
+		while(l+1<p)
+		{
+			if (l%2 == 0)
+			{
+				lh = (lh * P[da] + tab[l+1])%MOD;
+			}
+			if (p%2==1)
+			{
+				ph = (tab[p-1]*P[dp] + ph)%MOD;
+				dp += da;
+			}
+			l/=2; p/=2;
+			da *= 2;
+		}
+		return (lh * P[dp] + ph)%MOD;
+	}
+};
+
